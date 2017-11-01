583bd967f9b5eb31191a4f9d432b14e0f83798d0
Jirayupong <jirayupong@Monchais-MacBook-Pro.local>
2017-11-01 13:54:20 +0700

    add mlab mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,16 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp').then(()=>{
+//     console.log('1234');
+// },(err)=>{
+//     console.log(err);
+// });
+mongoose.connect('mongodb://monchai:seven77889900@ds243805.mlab.com:43805/demo-todo').then(()=>{
+    //success!
+},(err)=>{
+    console.log(err);
+});
 
 module.exports = {
     mongoose
