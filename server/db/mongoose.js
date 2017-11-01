d5bd7bbd697a5d310cd97aeca82b43cb07ef78e3
Jirayupong <jirayupong@Monchais-MacBook-Pro.local>
2017-11-01 16:04:43 +0700

    add NODE_ENV test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,23 +2,8 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
 
-switch(process.env.NODE_ENV){
-    case 'production':
-        mongoose.connect('mongodb://monchai:seven77889900@ds243805.mlab.com:43805/demo-todo').then(()=>{
-            //success!
-        },(err)=>{
-            console.log(err);
-        });
-        break;
-    case 'development':
-        mongoose.connect('mongodb://localhost:27017/TodoApp').then(()=>{
-            //success!
-        },(err)=>{
-            console.log(err);
-        });
-        break;
-}
 module.exports = {
     mongoose
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+if(env == 'development'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}else if(env == 'test'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}else if(env == 'production'){
+    process.env.MONGODB_URI = 'mongodb://monchai:seven77889900@ds243805.mlab.com:43805/demo-todo';
+}
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
