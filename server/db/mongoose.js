b1fa542a59676497b1e79d82faf1ae6746c03e19
Jirayupong <jirayupong@Monchais-MacBook-Pro.local>
2017-11-01 15:44:44 +0700

    add NODE_ENV production and development

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,18 +2,23 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp').then(()=>{
-    //success!
-},(err)=>{
-    console.log(err);
-});
 
-// mongoose.connect('mongodb://monchai:seven77889900@ds243805.mlab.com:43805/demo-todo').then(()=>{
-//     //success!
-// },(err)=>{
-//     console.log(err);
-// });
-
+switch(process.env.NODE_ENV){
+    case 'production':
+        mongoose.connect('mongodb://monchai:seven77889900@ds243805.mlab.com:43805/demo-todo').then(()=>{
+            //success!
+        },(err)=>{
+            console.log(err);
+        });
+        break;
+    case 'development':
+        mongoose.connect('mongodb://localhost:27017/TodoApp').then(()=>{
+            //success!
+        },(err)=>{
+            console.log(err);
+        });
+        break;
+}
 module.exports = {
     mongoose
 }
