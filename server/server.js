410bb8f8f742bc066e33a350ef4f63b5f8311ce6
Jirayupong <jirayupong@Monchais-MacBook-Pro.local>
2017-11-01 14:53:19 +0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -57,6 +58,29 @@ app.delete('/todos/:id',(req,res)=>{
         res.status(404).send();
     }
     Todo.findByIdAndRemove(req.params.id).then((todo)=>{
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send(todo);
+    }).catch(err=>{
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
         if(!todo){
             res.status(404).send();
         }
