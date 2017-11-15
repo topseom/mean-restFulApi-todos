22758eb452877552ec3efabd069815aa232d85bd
Jirayupong <jirayupong@Monchais-MacBook-Pro.local>
2017-11-15 21:04:53 +0700

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,5 @@
+//import { resolve } from 'url';
+
 const mongoose = require('mongoose');
 const validator = require('validator');
 const _ = require('lodash');
@@ -65,6 +67,29 @@ UserSchema.statics.findByToken = function(token){
         'tokens.token':token,
         'tokens.access':'auth'
     });  
+};
+
+UserSchema.statics.findByCredentials = function(email,password){
+    var User = this;
+    
+    return User.findOne({
+        'email': email
+    }).then(user=>{
+        if(!user){
+            return Promise.reject();
+        }
+        return new Promise((resolve,reject)=>{
+            bcrypt.compare(password,user.password,(err,res)=>{
+                if(res){
+                    resolve(user);
+                }else{
+                    reject();
+                }
+            });
+        }); 
+    });  
+
+    
 };
 
 UserSchema.pre('save',function(next){
--- server/routes/users.js
+++ server/routes/users.js
@@ -12,6 +12,17 @@ router.get("/users/me",authenticate,(req,res)=>{
     res.send(req.user);
 });
 
+router.post("/users/login",(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    User.findByCredentials(body.email,body.password).then(user=>{
+        user.generateAuthToken().then((token)=>{
+            res.header('x-auth',token).send(user); 
+        });
+    }).catch(e=>{
+        res.status(400).send();
+    });
+});
+
 router.post("/users",(req,res)=>{
     var body = _.pick(req.body,['email','password']);
     var user = new User(body);
