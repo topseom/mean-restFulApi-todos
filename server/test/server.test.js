29b043b5ffc8a5d6935fcc626a65c679277eb7d4
Jirayupong <jirayupong@Monchais-MacBook-Pro.local>
2017-11-19 18:39:05 +0700

    Add tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -313,3 +313,26 @@ describe('POST /users/login',()=>{
             });
     });
 });
+
+
+describe('DELTE /users/me/token',()=>{
+    it('should delete user token',(done)=>{
+        request(app)
+            .delete(`/api/users/me/token`)
+            .set('x-auth',users[0].tokens[0].token)
+            .expect(200)
+            .end((err,res)=>{
+                if(err){
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then(user=>{
+                    //console.log(user.tokens.length); 
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e=>{
+                    done();
+                })
+            });
+    });
+});
