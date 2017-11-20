a283ff8ba86df5dddbdb52bfc2ed5fc8e895cd8e
Jirayupong <jirayupong@Monchais-MacBook-Pro.local>
2017-11-20 07:45:37 +0700

    Add async await

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -7,65 +7,70 @@ const _ = require('lodash');
 
 var {Todo} = require('../models/todo');
 
-rounter.post('/todos',authenticate,(req,res)=>{
+rounter.post('/todos',authenticate, async (req,res)=>{
     var todo = new Todo({
         text:req.body.text,
         _creator:req.user._id
     });
-    todo.save().then((doc)=>{
+    try{
+        const doc = await todo.save();
         res.send(doc);
-    },(e)=>{
+    }catch(e){
         res.status(400).send(e);
-    });
+    };
+    
 });
-rounter.get('/todos/:id',authenticate,(req,res)=>{
+rounter.get('/todos/:id',authenticate, async (req,res)=>{
     if(!ObjectID.isValid(req.params.id)){
         res.status(404).send();
     }
-    Todo.findOne({
-        _id:req.params.id,
-        _creator:req.user._id
-    }).then((todo)=>{
+    try{
+        const todo = await Todo.findOne({
+            _id:req.params.id,
+            _creator:req.user._id
+        });
         if(!todo){
             res.status(404).send();
         }
         res.send(todo);
-    }).catch(err=>{
+    }catch(e){
         res.status(400).send();
-    });
+    }
 });
 
-rounter.get('/todos',authenticate,(req,res)=>{
-    Todo.find({
-        _creator: req.user._id
-    }).then((todos)=>{
+rounter.get('/todos',authenticate, async (req,res)=>{
+    try{
+        const todos = await Todo.find({
+            _creator: req.user._id
+        });
         if(!todos){
             res.status(404).send({});
         }
         res.send(todos);
-    }).catch(err=>{
-        res.status(400).send(err);
-    });
+    }catch(e){
+        res.status(400).send(e);
+    }
 });
 
-rounter.delete('/todos/:id',authenticate,(req,res)=>{
+rounter.delete('/todos/:id',authenticate,async (req,res)=>{
     if(!ObjectID.isValid(req.params.id)){
         res.status(404).send();
     }
-    Todo.findOneAndRemove({
-        _id:req.params.id,
-        _creator:req.user._id
-    }).then((todo)=>{
+    try{
+        const todo = await Todo.findOneAndRemove({
+            _id:req.params.id,
+            _creator:req.user._id
+        });
         if(!todo){
             res.status(404).send();
         }
         res.send(todo);
-    }).catch(err=>{
+    }catch(e){
         res.status(400).send();
-    });
+    }
 });
 
-rounter.patch('/todos/:id',authenticate,(req,res)=>{
+rounter.patch('/todos/:id',authenticate, async (req,res)=>{
     var id = req.params.id;
     var body = _.pick(req.body,['text','completed']);
     if(!ObjectID.isValid(id)){
@@ -78,17 +83,19 @@ rounter.patch('/todos/:id',authenticate,(req,res)=>{
         body.completed = false;
         body.completedAt = null;
     }
-    Todo.findOneAndUpdate({
-        _id:id,
-        _creator:req.user._id
-    },{$set:body},{new:true}).then((todo)=>{
+
+    try{
+        const todo = await Todo.findOneAndUpdate({
+            _id:id,
+            _creator:req.user._id
+        },{$set:body},{new:true});
         if(!todo){
             res.status(404).send();
         }
         res.send(todo);
-    }).catch(err=>{
+    }catch(e){
         res.status(400).send();
-    });
+    }
 });
 
 module.exports = rounter;
--- server/routes/users.js
+++ server/routes/users.js
@@ -12,36 +12,36 @@ router.get("/users/me",authenticate,(req,res)=>{
     res.send(req.user);
 });
 
-router.post("/users/login",(req,res)=>{
+router.post("/users/login",async (req,res)=>{
     var body = _.pick(req.body,['email','password']);
-    User.findByCredentials(body.email,body.password).then(user=>{
-        user.generateAuthToken().then((token)=>{
-            res.header('x-auth',token).send(user); 
-        });
-    }).catch(e=>{
-        res.status(400).send();
-    });
-});
-
-router.delete("/users/me/token",authenticate,(req,res)=>{
-    req.user.removeToken(req.token).then(()=>{
-        res.status(200).send();
-    },()=>{
-        res.status(400).send();
-    });
-});
-
-router.post("/users",(req,res)=>{
-    var body = _.pick(req.body,['email','password']);
-    var user = new User(body);
-    
-    user.save().then(user=>{
-        return user.generateAuthToken();
-    }).then((token)=>{
+    try{
+        const user = await User.findByCredentials(body.email,body.password);
+        const token = await user.generateAuthToken();
         res.header('x-auth',token).send(user); 
-    }).catch(err=>{
-        res.status(400).send(err);
-    });
+    }catch(e){
+        res.status(400).send();
+    }
+});
+
+router.delete("/users/me/token",authenticate, async (req,res)=>{
+    try{
+        await req.user.removeToken(req.token);
+        res.status(200).send();
+    }catch(e){
+        res.status(400).send();
+    }
+});
+
+router.post("/users",async (req,res)=>{
+    try{
+        const body = _.pick(req.body,['email','password']);
+        const user = new User(body);
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.header('x-auth',token).send(user); 
+    }catch(e){
+        res.status(400).send(e);
+    }
 });
 
 module.exports = router;
